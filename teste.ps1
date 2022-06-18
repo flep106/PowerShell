0f8b36c77b495ce475a34d9771a3a4ffa481cf58
flep106 <vandrilho@gmail.com>
2022-06-18 17:23:32 -0300

    usando o teste

--- teste.ps1
+++ teste.ps1
@@ -14,9 +14,16 @@ param(
 #>
 
 
-pwd > .\output.txt
-#gera error proposital
-cd $a
+# pwd > .\output.txt
+# #gera error proposital
+# cd $a
 
+$body_sem_cadastro = Get-ChildItem -Path "C:\temp\" -Name body-message.html | Get-Content | Out-String
+#$body_sem_cadastro2 = $body_sem_cadastro.
+Write-Host($body_sem_cadastro)
+
+$body_sem_cadastro.GetType()
+
+#Write-Host($body_sem_cadastro)
 
 $Error[0] > .\output-erro.txt
