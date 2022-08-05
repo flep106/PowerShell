801302fe3a04017cbdcdcebbceb846e4eb66126b
flep106 <vandrilho@gmail.com>
2022-08-05 20:26:36 -0300

    Adicionado script de SOAP CA SDM

---
+++ CASDMCreateTicket.ps1
@@ -0,0 +1,30 @@
+﻿param ([string]$Type='R', [string]$Category, [string]$Summary,  [string]$Description, [string]$Group)
+
+function createCASDMRequest($CASDMUsr, $URL, $Type, $Summary, $Category, $Group, $Description){
+    $svc = New-WebServiceProxy -Uri $URL
+    $wsUsername=$CASDMUsr
+    $passwd=Read-Host -Prompt 'Enter your CA ServiceDesk Manager Password' -AsSecureString
+    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwd)
+    $wsPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+    $sid = $svc.login($wsUsername,$wsPassword)
+    $customerHandle = $svc.getHandleForuserID($sid,$wsUsername)  # Using the creator user as customer
+    $creatorHandle = $customerHandle
+    $prop = ""
+    $attr = "persistent_id"
+    $requestHandle = ""
+    $requestNumber = ""
+    $summ=$Summary
+    $desc=$Description
+    [xml]$groupHandleReturn = $svc.doSelect($sid,"grp","last_name='$Group'",1,"persistent_id")
+    $groupHandle=$groupHandleReturn.UDSObjectList.UDSObject.Handle
+    [xml]$categoryHandleReturn = $svc.doSelect($sid,"pcat","sym='$Category'",1,"persistent_id")
+    $categoryHandle=$categoryHandleReturn.UDSObjectList.UDSObject.Handle
+    $attrVal = "customer", $customerHandle, "type",$Type,"summary",$summ,"description",$desc,"group",$groupHandle,"category",$categoryHandle,"priority",5
+    [xml]$newRequestHandle = $svc.createRequest($sid,$creatorHandle,$attrVal,$prop,"","id",[ref]$requestHandle, [ref]$requestNumber)
+    return [String]$requestNumber
+}
+
+$URL = $env:URLCASDM  # Register the CA ServiceDesk wsdl URI in an environment variable
+$CASDMUsr = $env:USERNAME # User is the current user
+$Ticket = createCASDMRequest $CASDMUsr $URL $Type $Summary $Category $Group $Description
+Write-Host (Get-Date -UFormat "%d-%h-%Y %H:%M:%S : [INFO] Call Request Type $Type created: $Ticket") -ForegroundColor Green;
--- teste.ps1
+++ teste.ps1
@@ -19,18 +19,43 @@ param(
 # #gera error proposital
 # cd $a
 
-$saida = 123;
+# $saida = 123;
 
-if ($saida -ne 0) {
-    $saida = 1
+# if ($saida -ne 0) {
+#     $saida = 1
+# }
+# try {
+#     adfdfw
+# }
+# catch {ls
+
+#     {"TESTE"}
+# }
+
+
+$SERVIDORES_HMG = @{ 
+
+APP01_CATALOG_XFLOW = "SAODOMINGOS"; 
+APP02_CATALOG_XFLOW = "SAODESIDERIO";
+APP03_INTEGRACAO = "SAOBRAS";
+STANDBY = "SAOBORJA"; 
 }
-try {
-    adfdfw
+
+$SRV_TESTE = @{
+TARRASQUE = "TARRASQUE"
+NOTE_SOLUTIS = "SLT-002595"
+SDM_DEV = "SDM-STANDALONE"
 }
-catch {
-    {"TESTE"}
+
+foreach ($item_chave in $SRV_TESTE.Keys) {
+   "Chave-> " + $item_chave + " Valor-> " + $SRV_TESTE[$item_chave]
 }
 
-
-
+#Write-Host $SRV_TESTE.SDM_DEV.GetType()
 #$Error[0] > .\output-erro.txt
+#
+$Senha = ConvertTo-SecureString -String 'V@ndrilho1' -AsPlainText -Force
+$PScredencial = New-Object -TypeName System.Management.Automation.PSCredential("Administrador\Administrador", $Senha)
+$Sessao = New-PSSession -ComputerName $SRV_TESTE.TARRASQUE -Credential $PScredencial
+Copy-Item  "\\SLT-002595\c$\temp\select.txt" -Destination "\\TARRASQUE\c$\temp" -ToSession $Sessao
+#
